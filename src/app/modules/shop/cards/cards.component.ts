15de106ffb6f43029b9c23acb2b44fdbe7414970
xebec19 <rohant946@gmail.com>
2021-11-23 20:14:04 +0530

    enhancing ui

--- src/app/modules/shop/cards/cards.component.ts
+++ src/app/modules/shop/cards/cards.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
 import { IProduct } from 'src/app/models/product.model';
+import { AppStateService } from 'src/app/services/app-state.service';
 import { RequestHandlerService } from 'src/app/services/request-handler.service';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+  styleUrls: ['./cards.component.scss'],
 })
 export class CardComponent implements OnInit {
   @Input()
@@ -15,17 +16,27 @@ export class CardComponent implements OnInit {
   @Input()
   filter: 'trending' | 'categories';
   products: IProduct[];
-  url:string;
-  constructor(private requestService: RequestHandlerService, private store: Store) { }
+  url: string;
+  constructor(
+    private requestService: RequestHandlerService,
+    private store: Store,
+    public appStateService: AppStateService
+  ) {}
 
   ngOnInit(): void {
     this.getProducts();
   }
   getProducts() {
-    switch(this.filter){
-      case 'categories': this.url = '/public/category?includeImg=true'; break;
-      case 'trending': this.url = '';break;
-      default: this.url = '/public/fetchProducts?limit=10'; break;
+    switch (this.filter) {
+      case 'categories':
+        this.url = '/public/category?includeImg=true';
+        break;
+      case 'trending':
+        this.url = '';
+        break;
+      default:
+        this.url = '/public/fetchProducts?limit=10';
+        break;
     }
     this.requestService
       .getRequest('/public/fetchProducts?limit=10')
@@ -36,10 +47,8 @@ export class CardComponent implements OnInit {
           }
         })
       )
-      .subscribe(
-        (response: any) => {
-          this.products = response.data;
-        }
-      );
+      .subscribe((response: any) => {
+        this.products = response.data;
+      });
   }
 }
--- src/app/shared/cards/cards.component.ts
+++ src/app/shared/cards/cards.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { IProduct } from 'src/app/models';
+import { AppStateService } from 'src/app/services/app-state.service';
 import { RequestHandlerService } from 'src/app/services/request-handler.service';
 
 @Component({
@@ -12,12 +13,12 @@ export class CardsComponent implements OnInit {
   @Input()
   mode: 'mobile' | 'desktop';
   products: IProduct[];
-  constructor(private requestService: RequestHandlerService) {}
+  constructor(private requestService: RequestHandlerService, public appStateService: AppStateService) {}
   ngOnInit(): void {
     this.getProducts();
   }
   getProducts() {
-    const url = (this.mode === 'mobile') ? '/public/fetchProducts?limit=10' : '/public/fetchProducts?limit=6'
+    const url = (this.mode === 'mobile') ? '/public/fetchProducts?limit=10' : '/public/fetchProducts?limit=8'
     this.requestService
       .getRequest(url)
       .pipe(
